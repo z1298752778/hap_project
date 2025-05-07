8ffa3bda7b309c750d4d08fa83ad9c7353fd6be1
zr <1298752778@qq.com>
2025-05-07 17:12:00 +0800

    init

---
+++ hap_ty_problem_report/src/components/Logs.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="block mt10 log">
+    <div class="content">
+      <div class="base">~/mdye</div>
+      <div class="log-list">
+        <div class="log-item" v-for="(log, index) in logs" :key="index">
+          <span class="time">{{ log.timeStr }}</span>
+          {{ log.content }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(["logs"]);
+const { logs } = props;
+</script>
